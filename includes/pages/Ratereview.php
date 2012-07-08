04efa00acc5380213170579e028c30f79ce2d93b
Svip <svip@diku.dk>
2012-07-08 17:52:23 +0200

    Using global functions instead.

--- includes/pages/Ratereview.php
+++ includes/pages/Ratereview.php
@@ -10,32 +10,30 @@ class PageRatereview extends Page {
 	}
 	
 	private function handleRating() {
-		global $DB, $auth;
-		
 		$id = $_GET['id'];
 		$rating = $_GET['rating'];
 		
 		// Check for sane input
 		if ( !$id || !is_numeric($id) || $id < 1 || !$rating 
-			|| !in_array($rating, array(1, -1)) || !$auth->isLoggedIn() ) {
+			|| !in_array($rating, array(1, -1)) || !gfGetAuth()->isLoggedIn() ) {
 			header('Location: /');
 			return;
 		}
 		
-		$userid = $auth->get_userdata('userid');
+		$userid = gfGetAuth()->get_userdata('userid');
 		
-		$DB->query("SELECT `id` FROM `reviewratings` WHERE `reviewid` = $id AND `userid` = $userid");
+		$i = gfDBQuery("SELECT `id` FROM `reviewratings` WHERE `reviewid` = $id AND `userid` = $userid");
 		
-		if ( $DB->get_num_rows() > 0 ) {
+		if ( gfDBGetNumRows($i) > 0 ) {
 			// already rated, so let's change their rating!
 			
-			$result = $DB->get_result();
+			$result = gfDBGetResult($i);
 			
-			$DB->query("UPDATE `reviewratings` SET `rating` = $rating WHERE `id` = {$result['id']}");
+			gfDBQuery("UPDATE `reviewratings` SET `rating` = $rating WHERE `id` = {$result['id']}");
 		} else {
 			// new rating!
 			
-			$DB->query("INSERT INTO `reviewratings` SET `reviewid` = $id, `userid` = $userid, `rating` = $rating");
+			gfDBQuery("INSERT INTO `reviewratings` SET `reviewid` = $id, `userid` = $userid, `rating` = $rating");
 		}
 		
 		header("Location: /?p=episode&review=$id#review-$id");
