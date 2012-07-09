cddb7c9ec4f4d0c2670931a074d7e76a4bba36af
Svip <svip@theinfosphere.org>
2012-07-09 10:34:46 +0200

    Added question about approval system

--- includes/pages/Faq.php
+++ includes/pages/Faq.php
@@ -14,6 +14,7 @@ class PageFaq extends Page {
 		'Cool cool, how do I become a reviewer?' => "You can register an account easily, however to become an actual reviewer, you will have to contact 'Svip' on <a href=\"http://peelified.com\">PEEL</a> with an example review and your username on here.",
 		"I saw this review, and I thought it wasn't very good.  Should it remain?" => "You can rate it if you have an account.  Any account can rate.",
 		'Can you tell me about the modes on each page?' => "Certainly.  There is currently 3 modes available; <b>Reviewer</b>, <b>No ratings</b> and <b>Default</b>.  <b>Default</b> is, as its name implies, the default viewing mode; full ratings and full reviews.  <b>No ratings</b> removes all numeral ratings from reviews and overview and only focuses on the written content of reviews.  Some people may find the numbers distraction for someone's opinion on an episode. <b>Reviewer</b> is a mode only accessible to reviewers, in this mode, only the reviews of the reviewer appears.  This is to avoid the context of other reviews and only review the episode from one's own standpoint.",
+		'What are the approval and disapproval of reviews?' => "Other users can disapprove or approve of another's review.  However, it is <em>not meant</em> to be agree and disagree with the review, it is to indicate that one think it is a well-written review or not.  If the reviewer updates his/her textual review, the approval/disapproval count will be reset.",
 	);
 	
 	protected function render ( ) {
